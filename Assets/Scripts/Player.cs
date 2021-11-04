703fcb1887e4e848e12fe8e2ded94295b950c450
X_Shuler_X <egor.pisanov@gmail.com>
2021-11-04 14:00:05 +0300

    Base setup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,40 @@ namespace Asteroids
     internal sealed class Player : MonoBehaviour
     {
         [SerializeField] private float _speed;
+        [SerializeField] private float _acceleration;
         [SerializeField] private float _hp;
         [SerializeField] private Rigidbody2D _bullet;
         [SerializeField] private Transform _barrel;
         [SerializeField] private float _force;
-        private Vector3 _move;
+        private Camera _camera;
+        private Ship _ship;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+            var moveTransform = new AccelerationMove(transform, _speed, _acceleration);
+            var rotation = new RotationShip(transform);
+            _ship = new Ship(moveTransform, rotation);
+        }
 
         private void Update()
         {
-            var deltaTime = Time.deltaTime;
-            var speed = deltaTime * _speed;
-            _move.Set(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
-            transform.localPosition += _move;
+            var direction = Input.mousePosition - _camera.WorldToScreenPoint(transform.position);
+            _ship.Rotation(direction);
+
+            _ship.Move(Input.GetAxis("Horizontal"),
+                Input.GetAxis("Vertical"),
+                Time.deltaTime);
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                _ship.AddAcceleration();
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                _ship.RemoveAcceleration();
+            }
 
             if (Input.GetButtonDown("Fire1"))
             {
